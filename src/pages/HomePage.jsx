fa8c863a3f86ba42ba61560d804ebd8b3a999f1a
Andre-tech671 <nyanjahiaandre@gmail.com>
2025-10-17 19:07:37 +0200

    Use data from the backend to generate the html

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,23 @@
 
 import React from 'react'
 import Header from '../components/Header';
-import { products } from '../../starting-files/data/products';
 import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './HomePage.css';
 
 function HomePage() {
 
-  axios.get('http://localhost:3000/api/products')
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
     .then((response)=>{
-      console.log(response.data);
+      setProducts(response.data);
     });
+  }, []);
+
+ 
     
   return (
     <>
